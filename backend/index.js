67b5706b67928575c11754aa3afbbf12da089ada
frank0810 <105295225+frank0810@users.noreply.github.com>
2024-05-11 17:09:20 +0200

    latest

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Errore di connessione a MongoDB:'));
-db.once('open', () => {
-  console.log('Connessione a MongoDB riuscita');
-});
 
 app.listen(PORT, () => {
     console.log(`Server in ascolto sulla porta ${PORT}`);
