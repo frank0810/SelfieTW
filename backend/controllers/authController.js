b44becbcf5533a1396022f8f9162c61207c23e4e
EBigi23 <75383058+EBigi23@users.noreply.github.com>
2024-07-12 18:03:24 +0200

    Logout e Navbar

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,8 +10,6 @@ exports.register = async (req, res) => {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    //const hashedPassword = await bcrypt.hash(password, 10);
-
     const newUser = new User({ firstName, lastName, email, password });
     await newUser.save();
 
